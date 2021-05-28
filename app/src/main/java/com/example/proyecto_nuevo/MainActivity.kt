e807c20df22a0a3afdcf58f9ea80887471c74bac
Braulio <braulioarguetamartinez3c2bda@gmail.com>
2021-05-27 19:59:50 -0500

    version 1

---
+++ app/src/main/java/com/example/proyecto_nuevo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.proyecto_nuevo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        setTheme(R.style.SplashTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+private fun hi(){
+
+
+}
+
+}
